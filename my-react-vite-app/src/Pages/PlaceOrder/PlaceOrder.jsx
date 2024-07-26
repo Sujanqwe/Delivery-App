e0c794491b95e56fe08c6dae3932f028d7781937
unknown <dutsujan07@gmail.com>
2024-07-27 01:22:39 +0530

    Food_Delivery_MERN

---
+++ my-react-vite-app/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Placeorder.css'
+const PlaceOrder = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+import './Placeorder.css'
+export default PlaceOrder
