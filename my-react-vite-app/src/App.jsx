d2d860b952bb4982fc4ef6543dbe09cfdb3efd2d
Sujanqwe <dutsujan@gmail.com>
2024-07-28 11:08:01 +0530

    sujan here

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Pages/Home/Home'
 import Cart from './Pages/Cart/Cart'
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
+import LoginPopup from './Components/LoginPopup/LoginPopup'
 const App = () => {
-  return (
-    <div className='app'>
-      <Navbar/>
 
-      
-      <Routes>
+  //  const[showLogin,setShowLogin]= useState(false)
+
+   return (
+     <>
+      {/* {showLogin?<LoginPopup/>:<></>} */}
+      <div className='app'>
+       <Navbar/>
+      {/* <Navbar setShowLogin={setShowLogin}/> */}
+      {/* <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/' element={<Cart/>} />
-        <Route path='/' element={<PlaceOrder/>} />
-      </Routes>
-    </div>
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>  */}
+      </div>
+      </>
   )
 }
 
---
+++ my-react-vite-app/src/Components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import'./LoginPopup.css'
+
+const LoginPopup = () => {
+  return (
+    <div className='login-popup'>
+      login
+    </div>
+  )
+}
+import'./LoginPopup.css'
+
+export default LoginPopup
